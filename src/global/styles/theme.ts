421c2ef3a7a0732aafc051aa031d3061aa778f66
Fellipe Neves <fllisilva.gok@rd.com.br>
2023-04-04 11:23:08 -0300

    style: adjust theme

--- src/global/styles/theme.ts
+++ src/global/styles/theme.ts
@@ -1,7 +1,18 @@
 export const APP_THEME = {
-  background: '#FFFFFF',
-  primary: '#000000',
-  secondary: '#7E7E7E',
-  shade: '#E5E5E5',
-  background_secondary: '#E8E8E8',
+  colors: {
+    background: '#FFFFFF',
+    primary: '#000000',
+    secondary: '#7E7E7E',
+    shade: '#E5E5E5',
+    background_secondary: '#E8E8E8',
+  },
+  fonts: {
+    primary: {
+      regular: 'Mulish-Regular',
+      bold: 'Mulish-Bold',
+    },
+    secondary: {
+      regular: 'Roboto-Regular',
+    },
+  },
 };
