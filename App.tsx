c01d0611742d922266552ea812805ac8473211ea
Fellipe Neves <fllisilva.gok@rd.com.br>
2023-04-04 15:52:20 -0300

    feature: created home screen

--- App.tsx
+++ App.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
 
-import {View} from 'react-native';
 import {ThemeProvider} from 'styled-components';
 
 import {APP_THEME} from './src/global/styles/theme';
-import Test from './src/assets/icons/delete.svg';
+import Home from './src/screens/Home';
 
 function App(): JSX.Element {
   return (
     <ThemeProvider theme={APP_THEME}>
-      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-        <Test fill="red" width={40} height={40} />
-      </View>
+      <Home />
     </ThemeProvider>
   );
 }
