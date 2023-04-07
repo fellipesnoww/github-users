ad9b18a1281b358a4eee5fe3facc38784b71c6e3
Fellipe Neves <fllisilva.gok@rd.com.br>
2023-04-07 15:53:07 -0300

    fix: adjust container type

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.TouchableOpacity`
+export const Container = styled.View`
   align-items: center;
   justify-content: space-between;
   background-color: ${({theme}) => theme.colors.background};
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import {Container} from './styles';
 import Header from '../../components/Header';
 import UserCard from '../../components/UserCard';
+import HeaderWithUser from '../../components/HeaderWithUser';
 
 export default function Users() {
   return (
     <Container>
       <Header />
+      <HeaderWithUser />
       {/* <UserList data={[]} /> */}
       <UserCard />
     </Container>
