a89ce1fd7de6c22df88f0a16a18c4a153371c40b
fellipesnoww <fellipe2.0e@gmail.com>
2023-05-13 14:28:00 -0300

    chore: add provider config

--- App.tsx
+++ App.tsx
@@ -4,11 +4,14 @@ import {ThemeProvider} from 'styled-components';
 
 import {APP_THEME} from './src/global/styles/theme';
 import Routes from './src/routes';
+import {UsersProvider} from './src/contexts/UsersProvider';
 
 function App(): JSX.Element {
   return (
     <ThemeProvider theme={APP_THEME}>
-      <Routes />
+      <UsersProvider>
+        <Routes />
+      </UsersProvider>
     </ThemeProvider>
   );
 }
