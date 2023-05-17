df1e8c3d0f50007977f70c05fdff8a133dc4e0a7
fellipesnoww <fellipe2.0e@gmail.com>
2023-05-17 11:18:37 -0300

    config: add firebase crashlytics

--- App.tsx
+++ App.tsx
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {ThemeProvider} from 'styled-components';
 
 import {APP_THEME} from './src/global/styles/theme';
 import Routes from './src/routes';
 import {UsersProvider} from './src/contexts/UsersProvider';
 import * as Sentry from '@sentry/react-native';
-
+import crashlytics from '@react-native-firebase/crashlytics';
 Sentry.init({ 
   dsn: 'https://79b659b980e94e60bd6dbeee92a7c0bf@o4505196482527232.ingest.sentry.io/4505196482592768',
 });
 
 function App(): JSX.Element {
+  useEffect(() => {
+    crashlytics().log('App mounted.');
+  }, []);
+
   return (
     <ThemeProvider theme={APP_THEME}>
       <UsersProvider>
