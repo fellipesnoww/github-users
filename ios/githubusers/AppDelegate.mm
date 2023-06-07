1f8bfb92231de03564dea26aaac8cbd24b19328e
fellipesnoww <fellipe2.0e@gmail.com>
2023-06-07 15:51:11 -0300

    fix: remove all references from react-native-splash-screen

--- ios/githubusers/AppDelegate.mm
+++ ios/githubusers/AppDelegate.mm
@@ -4,14 +4,11 @@
 
 #import <Firebase.h>
 
-#import "RNSplashScreen.h"
-
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   [FIRApp configure];
-  [RNSplashScreen show];
   self.moduleName = @"githubusers";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -7,19 +7,12 @@ import Register from '../screens/Register';
 import Repository from '../screens/Repository';
 import ModalRegister from '../screens/Register/ModalRegister';
 import {useUsers} from '../hooks/useUsers';
-import SplashScreen from 'react-native-splash-screen';
+
 
 const Stack = createNativeStackNavigator();
 
 function AppRoutes() {
   const {users, loadingUsers} = useUsers();
-
-  if (loadingUsers) {
-    SplashScreen.show();
-    return null;
-  } else {
-    SplashScreen.hide();
-  }
 
   const initialRouteName = users.length > 0 ? 'Home' : 'Register';
 
